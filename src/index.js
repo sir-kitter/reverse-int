0a5ea70419845c2765100e70ab66f61c6e316d32
sir-kitter <sire.kitter@gmail.com>
2020-10-01 05:51:02 +0300

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+	if(n === 0) return 0
+	if(n < 0) n = -n
+	let s = ''
+	while(n > 0) {
+		s += n % 10
+		n = Math.floor(n / 10)
+	}
+	return s || '0'
 }
